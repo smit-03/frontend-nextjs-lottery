c61e577acb0bf85cfecc78738fa0693707959364
Smit <smitpativala03@gmail.com>
2023-07-02 00:07:29 +0530

    Initial Commit

---
+++ components/LotteryEntrance.jsx
@@ -0,0 +1,115 @@
+import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+    const chainId = parseInt(chainIdHex);
+    const raffleAddress =
+        chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+    const [entranceFee, setEntranceFee] = useState("0");
+    const [numPlayers, setNumPlayers] = useState("0");
+    const [recentWinner, setRecentWinner] = useState("0");
+    const dispatch = useNotification();
+
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "enterRaffle",
+        params: {},
+        msgValue: entranceFee,
+    });
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+        msgValue: "",
+    });
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getNumberOfPlayers",
+        params: {},
+        msgValue: "",
+    });
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getRecentWinner",
+        params: {},
+        msgValue: "",
+    });
+
+    async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString();
+        const numOfPlayersFromCall = (await getNumberOfPlayers()).toString();
+        const recentWinnerFromCall = await getRecentWinner();
+        setEntranceFee(entranceFeeFromCall);
+        setNumPlayers(numOfPlayersFromCall);
+        setRecentWinner(recentWinnerFromCall);
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI();
+        }
+    }, [isWeb3Enabled]);
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1);
+        handleNewNotification(tx);
+        updateUI();
+    };
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Transaction Notification",
+            position: "topR",
+            icon: "bell",
+        });
+    };
+
+    return (
+        <div className="p-5">
+            <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
+            {raffleAddress ? (
+                <>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                        onClick={async () =>
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: error => console.log(error),
+                            })
+                        }
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-6 w-6 border-b-2 rounded-full"></div>
+                        ) : (
+                            "Enter Raffle"
+                        )}
+                    </button>
+                    <div>
+                        Entrance Fee:{" "}
+                        {ethers.utils.formatUnits(entranceFee, "ether")} ETH
+                    </div>
+                    <div>The current number of players is: {numPlayers}</div>
+                    <div>The most previous winner was: {recentWinner}</div>
+                </>
+            ) : (
+                <div>Please connect to a supported chain </div>
+            )}
+        </div>
+    );
+}
